402a3221dd20a7bbc423d491a70bda8b25f361b4
Muhammadqodir Azamov <muhammadqodirazamov@outlook.com>
2026-07-30 11:17:53 +0500

    port changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 3003,
     strictPort: true,
-    allowedHosts: ['tough-actually-imp.ngrok-free.app', 'e56b-185-139-138-222.ngrok-free.app'],
+    allowedHosts: ['tough-actually-imp.ngrok-free.app', 'e56b-185-139-138-222.ngrok-free.app', '149.102.143.196:3003'],
   },
 })
